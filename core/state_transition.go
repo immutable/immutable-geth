b809c222a8b423f24333063fd4e5bf2e66ea6eba
sergerad <serge.radinovich@immutable.com>
2025-03-11 12:25:26 +1300

    Fix unused import

--- core/state_transition.go
+++ core/state_transition.go
@@ -26,7 +26,6 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto/kzg4844"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/holiman/uint256"
 )
